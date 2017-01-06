c582ad670e2b56237dab70bdf8642ce4c728d2bb
crmon <crmon93@gmail.com>
2017-01-06 02:33:25 -0300

    Com sprites novos, sem bug de pular ou deslizar. bug na colisão dos objetos

---
+++ ProjetoA-v1.1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public Animator Anime;
+	public Rigidbody2D PlayerRigidBody;
+	public int forceJump;
+	public bool jump;
+	public bool slide;
+
+	//Verifica o chão
+	public bool grounded;
+	public LayerMask whatIsGround;
+	public Transform groundCheck;
+
+	//slide
+	public float slideTemp;
+	public float timeTemp;
+
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if ((Input.GetButtonDown("Jump")) && (grounded == true)){
+			PlayerRigidBody.AddForce (new Vector2(0,forceJump));
+			jump = true;
+			slide = false;
+
+		}
+
+		if ((Input.GetButtonDown ("Slide")) && (grounded == true)) {
+			slide = true;
+			timeTemp = 0;
+
+
+		}
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, 0.2f, whatIsGround);
+
+		if (slide == true) {
+			timeTemp += Time.deltaTime;
+			if (timeTemp >= slideTemp) {
+				slide = false;
+			}
+		}
+
+		Anime.SetBool ("jump", !grounded);
+		Anime.SetBool ("slide", slide);
+
+	}
+}
